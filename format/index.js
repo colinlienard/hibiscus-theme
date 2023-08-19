675075168927bfde721c2226f01ad728b7d4fff6
Colin Lienard <colin.lienard87@gmail.com>
2023-08-19 21:28:57 +0200

    update

--- format/index.js
+++ format/index.js
@@ -1,16 +1,18 @@
 import fs from 'fs';
 
-const PATH = './themes/theme.json';
+const PATH = './themes/Glace-color-theme.json';
 
 const json = fs.readFileSync(PATH, 'utf8');
 const object = JSON.parse(json);
+
+// Sort by alphabetical order
 const sortedColors = {};
 Object.keys(object.colors)
   .sort()
   .forEach((key) => {
     sortedColors[key] = object.colors[key];
   });
+
 object.colors = sortedColors;
-console.log(object);
 
 fs.writeFileSync(PATH, JSON.stringify(object));
