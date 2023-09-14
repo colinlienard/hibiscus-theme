f380e9b86023b6752d47fd27d2a60cc7582f9993
Colin Lienard <colin.lienard87@gmail.com>
2023-09-14 21:11:31 +0200

    feat: use bun

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import { writeFileSync } from 'fs';
 import { getTheme } from './theme';
 
 try {
   const theme = getTheme('Hibiscus');
-  writeFileSync('themes/generated.json', JSON.stringify(theme));
+  await Bun.write('themes/generated.json', JSON.stringify(theme));
   console.log('\x1b[32m', '✔ Theme generated');
 } catch (e) {
   console.error('\x1b[31m', e);
